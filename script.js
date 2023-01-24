9531c70348d9f84038e81559e960d2d3b380bab0
SaikiranGadangiwar <106570439+SaikiranGadangiwar@users.noreply.github.com>
2023-01-24 11:29:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+// Remove non-alphanumeric characters and convert to lowercase
+  const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  // Reverse the cleaned string and check if it is equal to the original
+  return cleaned === cleaned.split('').reverse().join('');
 }
 module.exports = palindrome
